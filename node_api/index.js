d8f427e9f9dee660a83686c72bc68d2eb880accd
The Doctor <xX-TheDoctor-Xx@users.noreply.github.com>
2020-09-29 12:10:32 +0100

    Update index.js

--- node_api/index.js
+++ node_api/index.js
@@ -1,11 +1,11 @@
 const Gpio = require('onoff').Gpio;
-const led = new Gpio(11, 'out');
+const led = new Gpio(4, 'out');
 
 var value = false;
 
 while (true)
 {
-    led.writeSync(value);
+    led.writeSync(value ? 1 : 0);
     value = !value;
     console.log((value ? "on" : "off"));
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 1000);
