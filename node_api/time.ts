f8bd2c36d2cf5afe142c47133f4b33654d55a78e
The Doctor <xX-TheDoctor-Xx@users.noreply.github.com>
2020-10-02 14:27:09 +0100

    Update time.ts

--- node_api/time.ts
+++ node_api/time.ts
@@ -21,12 +21,12 @@ export class Time
     {
         this.date = new Date();
 
-        if (!this.showSeconds)
-        {
+        //if (!this.showSeconds)
+        //{
             var hours = this.date.getHours();
             if (hours <= 10)
             {
-                var hoursArr = SplitNumber(this.date.getHours());
+                var hoursArr = SplitNumber(hours);
                 this.H1 = hoursArr[0];
                 this.H2 = hoursArr[1];
             }
@@ -39,7 +39,7 @@ export class Time
             var minutes = this.date.getMinutes();
             if (minutes <= 10)
             {
-                var minutesArr = SplitNumber(this.date.getMinutes());
+                var minutesArr = SplitNumber(minutes);
                 this.M1 = minutesArr[0];
                 this.M2 = minutesArr[1];
             }
@@ -48,17 +48,17 @@ export class Time
                 this.M1 = 0;
                 this.M2 = minutes;
             }
-        }
-        else
-        {
-            var seconds = SplitNumber(this.date.getSeconds());
-            this.H1 = seconds[0];
-            this.H2 = seconds[1];
+        //}
+        //else
+        // {
+        //     var seconds = SplitNumber(this.date.getSeconds());
+        //     this.H1 = seconds[0];
+        //     this.H2 = seconds[1];
 
-            var milliseconds = SplitNumber(this.date.getMilliseconds());
-            this.M1 = milliseconds[0];
-            this.M2 = milliseconds[1];
-        }
+        //     var milliseconds = SplitNumber(this.date.getMilliseconds());
+        //     this.M1 = milliseconds[0];
+        //     this.M2 = milliseconds[1];
+        // }
     }
 
     handleDisplay() // millisecond
