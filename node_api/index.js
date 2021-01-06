bb9e4ae831fe113149559545246f413a491bb2fc
GuilhermeStrice <GuilhermeStrice@users.noreply.github.com>
2021-01-06 15:16:37 +0000

    mcp test

--- node_api/index.js
+++ node_api/index.js
@@ -1,24 +1,20 @@
-var MCP23017 = require('node-mcp23017');
- 
-var mcp = new MCP23017({
-  address: 0x20, //default: 0x20
-  device: '/dev/i2c-1', // '/dev/i2c-1' on model B | '/dev/i2c-0' on model A
-  debug: true //default: false
-});
- 
-/*
-  By default all GPIOs are defined as INPUTS.
-  You can set them all the be OUTPUTs by using the pinMode-Methode (see below),
-  You can also disable the debug option by simply not passing it to the constructor
-  or by setting it to false
-*/
- 
-//set all GPIOS to be OUTPUTS
-for (var i = 0; i < 16; i++) {
-  mcp.pinMode(i, mcp.OUTPUT);
-  //mcp.pinMode(i, mcp.INPUT); //if you want them to be inputs
-  //mcp.pinMode(i, mcp.INPUT_PULLUP); //if you want them to be pullup inputs
-}
- 
-mcp.digitalWrite(0, mcp.HIGH); //set GPIO A Pin 0 to state HIGH
-mcp.digitalWrite(0, mcp.LOW); //set GPIO A Pin 0 to state LOW
+var MCP23017 = require("./mcp23017");
+
+var my_mcp = new MCP23017(1, 0);
+
+my_mcp.setPinDirection(0, 0);
+my_mcp.setPinDirection(1, 0);
+my_mcp.setPinDirection(2, 0);
+my_mcp.setPinDirection(3, 0);
+my_mcp.setPinDirection(4, 0);
+my_mcp.setPinDirection(5, 0);
+my_mcp.setPinDirection(6, 0);
+my_mcp.setPinDirection(7, 0);
+my_mcp.setPinDirection(8, 0);
+my_mcp.setPinDirection(9, 0);
+my_mcp.setPinDirection(10, 0);
+my_mcp.setPinDirection(11, 0);
+my_mcp.setPinDirection(12, 0);
+my_mcp.setPinDirection(13, 0);
+my_mcp.setPinDirection(14, 0);
+my_mcp.setPinDirection(15, 0);
