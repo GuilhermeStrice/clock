787f9320619aca1abc433577b06afd166ecf2c43
GuilhermeStrice <GuilhermeStrice@users.noreply.github.com>
2021-01-14 11:52:52 +0000

    removed build folder

--- node_api/build/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Low = exports.High = void 0;
-var High = 1;
-exports.High = High;
-var Low = 0;
-exports.Low = Low;
--- node_api/build/util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SplitNumber = void 0;
-function SplitNumber(number) {
-    var digits = number.toString().split('');
-    var realDigits = digits.map(Number);
-    return realDigits;
-}
-exports.SplitNumber = SplitNumber;
