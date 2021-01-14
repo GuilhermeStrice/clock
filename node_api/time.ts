902c5c99a9eade24390d23f05e78c24ab8132741
GuilhermeStrice <GuilhermeStrice@users.noreply.github.com>
2021-01-14 11:52:25 +0000

    update

--- node_api/time.ts
+++ node_api/time.ts
@@ -2,7 +2,6 @@ import { SplitNumber } from "./util";
 
 export class Time
 {
-    date: Date = new Date();
     //showSeconds: boolean = false;
 
     H1: number = 0;
@@ -12,14 +11,14 @@ export class Time
 
     update()
     {
-        this.date = new Date();
+        var date = new Date();
 
         //if (!this.showSeconds)
         //{
-            var hours = this.date.getHours();
+            var hours = date.getHours();
             var hoursArr = SplitNumber(hours);
 
-            var minutes = this.date.getMinutes();
+            var minutes = date.getMinutes();
             var minutesArr = SplitNumber(minutes);
             this.H1 = hoursArr[0];
             this.H2 = hoursArr[1];
@@ -28,11 +27,11 @@ export class Time
         //}
         //else
         // {
-        //     var seconds = SplitNumber(this.date.getSeconds());
+        //     var seconds = SplitNumber(date.getSeconds());
         //     this.H1 = seconds[0];
         //     this.H2 = seconds[1];
 
-        //     var milliseconds = SplitNumber(this.date.getMilliseconds());
+        //     var milliseconds = SplitNumber(date.getMilliseconds());
         //     this.M1 = milliseconds[0];
         //     this.M2 = milliseconds[1];
         // }
